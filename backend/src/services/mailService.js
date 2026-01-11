b5cbee9e54ca22f2ba40a496a559903211c49f53
Himanshu Kumar <himanshujaiswal8448@gmail.com>
2026-01-12 02:18:49 +0530

    some change

--- backend/src/services/mailService.js
+++ backend/src/services/mailService.js
@@ -1,22 +1,18 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html }) => {
-  // 🔴 DEBUG — ADD HERE
-  console.log("DEBUG ENV:", {
-    EMAIL_USER: process.env.EMAIL_USER,
-    EMAIL_PASS_EXISTS: !!process.env.EMAIL_PASS,
-  });
-
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
 
   await transporter.sendMail({
-    from: `"Student Grievance Portal" <${process.env.EMAIL_USER}>`,
+    from: `"Student Grievance Portal" <${process.env.SMTP_USER}>`,
     to,
     subject,
     html,
